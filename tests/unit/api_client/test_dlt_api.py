94aab58e3b02141bf64e751a23c1460ba6bf52f3
dustinvannoy-db <dustin.vannoy@databricks.com>
2024-07-13 18:25:33 -0700

    Fix unit tests and add development mode by default

--- tests/unit/api_client/test_dlt_api.py
+++ tests/unit/api_client/test_dlt_api.py
@@ -1,7 +1,5 @@
 import pytest
-import re
-import os
-from mock import Mock, ANY
+from mock import ANY
 from tests.unit.api_client.api_test_base import ApiTestBase
 from dbt.adapters.databricks.api_client import DltApi
 
@@ -9,66 +7,6 @@ class TestDltApi(ApiTestBase):
     @pytest.fixture
     def api(self, session, host):
         return DltApi(session, host, 1, 2)
-
-    @pytest.fixture
-    def integration_session(self):
-        import os
-        import requests
-
-        host = os.getenv("DBT_DATABRICKS_HOST")
-        token = os.getenv("DBT_DATABRICKS_TOKEN")
-
-        auth = ('token', token)
-        headers = {'Host': host, 'Content-Type': 'application/json'}
-
-        my_session = requests.Session()
-        my_session.auth = auth
-        my_session.headers = headers
-        return my_session
-
-    def test_create_integration__200(self, api, session):
-        import os
-        import requests
-
-        host = os.getenv("DBT_DATABRICKS_HOST")
-        token = os.getenv("DBT_DATABRICKS_TOKEN")
-
-        auth = ('token', token)
-        headers = {'Host': host, 'Content-Type': 'application/json'}
-
-        my_session = requests.Session()
-        my_session.auth = auth
-        my_session.headers = headers
-
-        my_name = "my_dlt_pipeline6"
-        my_notebook_path = "/Repos/dustin.vannoy@databricks.com/workshop_production_delta_official/notebooks/02-DLT for Data Engineering Pipelines (Python)"
-        my_catalog = "main"
-        my_schema = "dustin_vannoy"
-
-        my_api = DltApi(my_session, host, 1, 2)
-        uuid_regex = re.compile('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\Z')
-        assert bool(uuid_regex.match(my_api.create(my_name, my_notebook_path, my_catalog, my_schema)))
-
-    def test_get_integration__200(self, api, integration_session):
-        my_name = "my_dlt_pipeline6"
-        my_notebook_path = "/Repos/dustin.vannoy@databricks.com/workshop_production_delta_official/notebooks/02-DLT for Data Engineering Pipelines (Python)"
-
-        host = os.getenv("DBT_DATABRICKS_HOST")
-        my_api = DltApi(integration_session, host, 1, 2)
-        uuid_regex = re.compile('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\Z')
-        assert bool(uuid_regex.match(my_api.get_pipeline(my_name)['pipeline_id']))
-        # assert my_api.get_pipeline(my_name, my_notebook_path) == {"pipeline_id": "pipeline_id"}
-
-    def test_update_integration__200(self, api, integration_session):
-        my_pipeline_id = "0e116d3-9c36-4a1f-ab04-ad943b69b3e3"
-        my_name = "my_dlt_pipeline6"
-        my_notebook_path = "/Repos/user@databricks.com/example-repo/DLT_job"
-        my_catalog = "main"
-        my_schema = "dustin_vannoy"
-
-        host = os.getenv("DBT_DATABRICKS_HOST")
-        my_api = DltApi(integration_session, host, 1, 2)
-        assert my_api.update(my_pipeline_id, my_name, my_notebook_path, my_catalog, my_schema) == "pipeline_id"
 
     def test_create___200(self, api, session, host):
         my_name = "my_dlt_pipeline"
@@ -81,7 +19,20 @@ class TestDltApi(ApiTestBase):
         assert api.create(my_name, my_notebook_path, my_catalog, my_schema) == "pipeline_id"
         session.post.assert_called_once_with(
             f"https://{host}/api/2.0/pipelines",
-            json={'name': 'my_dlt_pipeline', 'catalog': 'main', 'target': 'dustin_vannoy', 'libraries': [{'notebook': {'path': '/Repos/user@databricks.com/example-repo/DLT_job'}}], 'continuous': ANY, 'edition': ANY, 'configuration': ANY, 'serverless': True},
+            json={'name': 'my_dlt_pipeline', 'catalog': 'main', 'target': 'dustin_vannoy', 'libraries': [{'notebook': {'path': '/Repos/user@databricks.com/example-repo/DLT_job'}}], 'development': ANY, 'continuous': ANY, 'edition': ANY, 'configuration': ANY, 'clusters': ANY},
+            params=None,
+        )
+
+    def test_get_200(self, api, session, host):
+        my_name = "my_dlt_pipeline"
+        session.get.return_value.status_code = 200
+        session.get.return_value.json.return_value = {
+            "statuses": [{"name": "my_dlt_pipeline", "pipeline_id": "pipeline_id"}]}
+
+        assert api.get_pipeline(my_name) == "pipeline_id"
+        session.get.assert_called_once_with(
+            f"https://{host}/api/2.0/pipelines?filter=name LIKE 'my_dlt_pipeline'",
+            json={},
             params=None,
         )
 
@@ -90,16 +41,16 @@ class TestDltApi(ApiTestBase):
         my_notebook_path = "/Repos/user@databricks.com/example-repo/DLT_job"
         my_catalog = "main"
         my_schema = "dustin_vannoy"
-        session.post.return_value.status_code = 200
-        session.post.return_value.json.return_value = {"pipeline_id": "pipeline_id"}
+        session.put.return_value.status_code = 200
+        session.put.return_value.json.return_value = {"pipeline_id": "pipeline_id"}
 
-        assert api.update(my_name, my_notebook_path, my_catalog, my_schema) == "pipeline_id"
-        session.post.assert_called_once_with(
+        assert api.update("pipeline_id", my_name, my_notebook_path, my_catalog, my_schema) is None
+        session.put.assert_called_once_with(
             f"https://{host}/api/2.0/pipelines/pipeline_id",
             json={'name': 'my_dlt_pipeline', 'catalog': 'main', 'target': 'dustin_vannoy',
                   'libraries': [{'notebook': {'path': '/Repos/user@databricks.com/example-repo/DLT_job'}}],
-                  'continuous': ANY, 'edition': ANY, 'configuration': ANY, 'serverless': True},
+                  'development': ANY, 'continuous': ANY, 'edition': ANY, 'configuration': ANY, 'clusters': ANY},
             params=None,
         )
-        pass
+
 
